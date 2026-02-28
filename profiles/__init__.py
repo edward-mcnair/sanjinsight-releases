074f9bd18ca6a14a4a128a8d77062dc13aed75e2
edward-mcnair <edward@microsanj.com>
2026-02-28 13:00:19 -0800

    Fix: handle empty directories in PyInstaller spec

--- profiles/__init__.py
+++
@@ -1,3 +0,0 @@
-from .profiles        import MaterialProfile, BUILTIN_PROFILES, PROFILE_REGISTRY
-from .profile_manager import ProfileManager, is_protected, SOURCE_BUILTIN, SOURCE_DOWNLOADED, SOURCE_USER
-from .downloader      import ProfileDownloader
